25bf7808ebf7914cd3d36030381338ecd4f0e075
Dennie <oculareo@gmail.com>
2024-02-06 07:49:26 -0500

    Added selected hero component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { Hero } from '../interfaces/hero';
 import { HEROES } from '../mock-heroes';
-import {
-  /* . . . */
-  NgFor,
-  /* . . . */
-} from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-heroes',
-  imports: [ NgFor, ],
+  imports: [ FormsModule, NgFor, NgIf, UpperCasePipe ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
   heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
